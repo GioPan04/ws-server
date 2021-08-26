8bf3cdc64dc8d764715b9b08f56b2a283ed0a09e
Gioele Pannetto <gioele@pannetto.com>
2021-08-26 15:14:38 +0200

    handle login and logout

--- src/ws/server.ts
+++ src/ws/server.ts
@@ -3,7 +3,8 @@ import EventEmitter from 'events';
 
 interface ChatServerEvents<U> {
   'connection': (user: U) => void;
-  'message': (data: any) => void;
+  'message': (message: string, user: U) => void;
+  'logout': (user: U) => void;
 }
 
 export declare interface ChatServer<U> {
@@ -15,14 +16,39 @@ export class ChatServer<U> extends EventEmitter {
 
   private server: Server;
   readonly users: U[] = [];
+
+  authMiddleware: (data: any) => U;
   
-  constructor() {
+  constructor({ authMiddleware }: IChatServer<U>) {
     super();
+
+    this.authMiddleware = authMiddleware;
+
     this.server = new Server({ port: 8080 });
     this.server.on('connection', this.onConnection);
   }
 
   private onConnection(ws: WebSocket) {
-    // this.emit('connection')
+    const user = this.authMiddleware(ws);
+    this.users.push(user);
+    this.emit('connection', user);
+    ws.on('message', (data) => this.onMessage(data, user));
+    ws.on('close', () => this.logout(user));
+  }
+
+  private onMessage(data: WebSocket.Data, user: U) {
+    this.emit('message', data.toString(), user);
+  }
+
+  private logout(user: U) {
+    const i = this.users.indexOf(user);
+    if(i === -1) return;
+    this.users.splice(i, 1);
+    this.emit('logout', user);
   }
 }
+
+interface IChatServer<U> {
+  authMiddleware: (data: any) => U;
+}
+
